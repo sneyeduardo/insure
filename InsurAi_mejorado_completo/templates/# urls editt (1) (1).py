817d7094dfabdb25ddb96f5afba44053ec628bc1
sneyeduardo <sneyeduardo4@gmail.com>
2026-03-18 17:08:01 -0400

    Delete InsurAi_mejorado_completo/templates/# urls editt (1) (1).py

--- InsurAi_mejorado_completo/templates/# urls editt (1) (1).py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # ... tus otras rutas ...
-    path('api/guardar-perfil/', views.guardar_perfil, name='api_guardar_perfil'),
-    path('api/listar-perfiles/', views.listar_perfiles, name='api_listar_perfiles'),
-]
