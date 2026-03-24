2a3840bd60205741be442e96bcb0a9acf2cc2f5d
sneyeduardo <sneyeduardo4@gmail.com>
2026-03-24 14:42:21 -0400

    Update settings.py

--- InsurAi_mejorado_completo/inzurai_project/settings.py
+++ InsurAi_mejorado_completo/inzurai_project/settings.py
@@ -13,10 +13,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
