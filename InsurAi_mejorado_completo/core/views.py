3c962f1aa837c13110c60e84eadb42ae5d3553eb
sneyeduardo <sneyeduardo4@gmail.com>
2026-03-24 20:20:13 -0400

    Update views.py

--- InsurAi_mejorado_completo/core/views.py
+++ InsurAi_mejorado_completo/core/views.py
@@ -18,7 +18,7 @@ from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-
+from django.conf import settings
 
 
 
