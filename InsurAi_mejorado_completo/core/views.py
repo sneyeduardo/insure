c0f09d63c4661984d8b520d21292993f09bdc554
sneyeduardo <sneyeduardo4@gmail.com>
2026-03-24 20:32:40 -0400

    Add HTTPBasicAuth import to views.py

--- InsurAi_mejorado_completo/core/views.py
+++ InsurAi_mejorado_completo/core/views.py
@@ -20,6 +20,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
+from requests.auth import HTTPBasicAuth
 
 
 
